634af6c90645cef663fc647b0e176523a820dd8e
Ties Stuij <ties@stuij.se>
2014-07-30 15:25:56 +0200

    remove comment fluff

--- src/KillGoggles.cpp
+++ src/KillGoggles.cpp
@@ -40,28 +40,3 @@ void KillGoggles::draw() {
         ofPopStyle();
     }
 }
-
-
-
-
-// create a rectangle around the face      
-//rectangle(frame, face_i, CV_RGB(255, 255 ,255), 1);
-
-// if good prediction : > threshold 
-//if (predicted_confidence > _prediction_confidence) {
-// sprintf(sTmp,"+ prediction ok = %s (%d) confiance = (%d)",people[prediction].c_str(),prediction,(int)predicted_confidence);
-	
-// display name of the guy on the picture
-//    string box_text;
-//    box_text = "Id=" + _names[prediction];
-            
-//    int pos_x = std::max(face_i.tl().x - 10, 0);
-//    int pos_y = std::max(face_i.tl().y - 10, 0);			   
-//    putText(gray, box_text, Point(pos_x, pos_y), FONT_HERSHEY_PLAIN, 1.0, CV_RGB(255,255,255), 1.0);        		
-//} else {		
-// trace is commented to speed up
-//sprintf(sTmp,"- prediction too low = %s (%d) confiance = (%d)",people[prediction].c_str(),prediction,(int)predicted_confidence);
-//trace((string)(sTmp));
-//}
-
-
